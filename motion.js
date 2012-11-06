2d9f0e33a2189dd9a0b825bc0fb2844d2b4708f7
Afshin Mehrabani <afshin.meh@gmail.com>
2012-11-06 19:09:27 +0330

    change HelloWord example

--- motion.js
+++ motion.js
@@ -1,5 +1,6 @@
 /*
  * motion.js 0.1.0
+ * http://usablica.github.com/motion.js
  * MIT licensed
  *
  * Copyright (C) 2012 usabli.ca - Afshin Mehrabani (afshin.meh@gmail.com)
@@ -15,7 +16,7 @@ var MotionJs = motionjs = (function() {
      * @param {Array} motion_obj
      * @api public
      */
-    function Transition(motion_obj) {
+    function transition(motion_obj) {
         for (var i = 0, motion_objects_len = motion_obj.length; i < motion_objects_len; i++) {
             var item = motion_obj[i];
             //get all objects with given selector from DOM
@@ -27,18 +28,39 @@ var MotionJs = motionjs = (function() {
                     var actor_object = motion_item[j];
                     //first, add the motion.js css class to the item 
                     actor_object.className = "motion_js_transition_all";
-                    //do the styles
-                    actor_object.style.left = item.left;
-                    actor_object.style.top = item.top;
-                    actor_object.style["-webkit-transition-duration"] = item.duration || DEFAULT_ANIMATIONS_DURATION;
+                    //set the styles
+                    _set_actor_styles(actor_object, item.style);
+                    //set transition duration 
+                    _set_transition_duration(actor_object, item.duration);
                 }
             }
-        };
+        }
     };
+    
+    /**
+     * Used for setting transition duration
+     *
+     * @param {Object} actor_object
+     * @api private
+     */
+    function _set_transition_duration(actor_object, duration) {
+        actor_object.style["-webkit-transition-duration"] = duration || DEFAULT_ANIMATIONS_DURATION;
+    }
+
+    /**
+     * Used for setting actor style
+     *
+     * @param {Object} actor_object
+     * @api private
+     */
+    function _set_actor_styles(actor_object, styles) {
+        actor_object.style.left = styles.left;
+        actor_object.style.top = styles.top;
+    }
 
     //API
     return {
         //Public API
-        transition: Transition
+        transition: transition
     };
 })();
